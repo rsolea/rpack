92f0d901f009ea6ca8ee039067d71136e11235ec
Eustaquio Rangel <taq@eustaquiorangel.com>
2010-11-12 14:41:42 -0200

    Checking for the given name.

--- lib/rpack.rb
+++ lib/rpack.rb
@@ -5,10 +5,15 @@ require "optparse"
 
 class Rpack
    def initialize
-      parse_options
+      @pattern = ARGV[0]
+      @options_parser, @options = parse_options
+      if @pattern.nil? || @pattern.size<1
+         puts "No name given"
+         puts @options_parser
+         exit
+      end
 
       @config  = YAML.load(File.open(File.dirname(__FILE__)+"/../config/config.yml"))
-      @pattern = ARGV[0]
       @list    = filelist
       p @pattern
       p @config
@@ -16,26 +21,36 @@ class Rpack
    end
 
    def parse_options 
-      @options = []
-      OptionParser.new do |opts|
-         opts.banner = "Usage: rpack <name> [options]"
-         opts.on("-a","--all")         { @options = %w(controller model view helper mailer migration 
-                                                       unit functional integration performance fixture 
-                                                       route)}
-
-         opts.on("-c","--controller")  { @options << "controller" }
-         opts.on("-m","--model")       { @options << "model"      }
-         opts.on("-v","--view")        { @options << "view"       }
-         opts.on("-h","--helper")      { @options << "helper"     }      
-         opts.on("-l","--mailer")      { @options << "mailer"     }
-         opts.on("-g","--migration")   { @options << "migration"  }
-         opts.on("-u","--unit")        { @options << "unit"       }
-         opts.on("-f","--functional")  { @options << "functional" }
-         opts.on("-i","--integration") { @options << "integration"}
-         opts.on("-p","--performance") { @options << "performance"}
-         opts.on("-x","--fixture")     { @options << "fixture"    }
-         opts.on("-r","--route")       { @options << "route"      }
-      end.parse!
+      options = []
+      parser  = nil
+      full_options = %w(controller model view helper mailer migration 
+                        unit functional integration performance fixture 
+                        route)
+      begin
+         OptionParser.new do |opts|
+            parser = opts
+            opts.banner = "Usage: rpack <name> [options]"
+            opts.on("-a","--all")         { options = full_options  }
+            opts.on("-c","--controller")  { options << "controller" }
+            opts.on("-m","--model")       { options << "model"      }
+            opts.on("-v","--view")        { options << "view"       }
+            opts.on("-h","--helper")      { options << "helper"     }      
+            opts.on("-l","--mailer")      { options << "mailer"     }
+            opts.on("-g","--migration")   { options << "migration"  }
+            opts.on("-u","--unit")        { options << "unit"       }
+            opts.on("-f","--functional")  { options << "functional" }
+            opts.on("-i","--integration") { options << "integration"}
+            opts.on("-p","--performance") { options << "performance"}
+            opts.on("-x","--fixture")     { options << "fixture"    }
+            opts.on("-r","--route")       { options << "route"      }
+         end.parse!
+         options = full_options if options.size<1
+      rescue => e
+         puts "Error: #{e}"
+         puts parser
+         exit
+      end
+      [parser,options]
    end
 
    def filelist
